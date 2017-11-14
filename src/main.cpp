3513244fcaba2504ed3454724773867a03bb8d06
Jay Young <yangjian@patsnap.com>
2017-11-14 23:33:17 +0800

    [ADD]ability to slow down and  change lane when car in front

--- src/main.cpp
+++ src/main.cpp
@@ -219,7 +219,7 @@ int main() {
   std::cout<<map_waypoints_y.size();
 
   int lane=1;
-  double ref_vel=49.5;
+  double ref_vel=0;
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane,&ref_vel](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
@@ -257,11 +257,44 @@ int main() {
             double end_path_d = j[1]["end_path_d"];
 
             // Sensor Fusion Data, a list of all other cars on the same side of the road.
-            auto sensor_fusion = j[1]["sensor_fusion"];
+            vector<vector<double>> sensor_fusion = j[1]["sensor_fusion"];
 
             int prev_size=previous_path_x.size();
 
             //start
+
+            if (prev_size>0){
+              car_s=end_path_s;
+            } else {
+              ref_vel = car_speed;
+            }
+
+            bool too_close=false;
+
+            for(auto perceived_car: sensor_fusion){
+              float d=perceived_car[6];
+              if(d<(2+4*lane+2) && d > (2+4*lane-2)){
+                double vx=perceived_car[3];
+                double vy=perceived_car[4];
+                double check_speed=sqrt(vx*vx+vy*vy);
+                double check_car_s=perceived_car[5];
+
+                check_car_s+=((double)prev_size*.02*check_speed);
+
+                if (check_car_s>car_s && check_car_s-car_s<30){
+                  too_close=true;
+                  // ref_vel=29.5;
+                }
+              }
+            }
+
+            if (too_close){
+              ref_vel-=0.3;
+              lane=0;
+            }else if(ref_vel<49.5){
+              ref_vel+=0.3;
+            }
+
             vector<double> ptsx;
             vector<double> ptsy;
 
@@ -317,8 +350,6 @@ int main() {
             }
             tk::spline spline;
 
-          cout<<ptsx<<endl;
-          cout<<ptsy<<endl;
             spline.set_points(ptsx, ptsy);
 
             vector<double> next_x_vals;
